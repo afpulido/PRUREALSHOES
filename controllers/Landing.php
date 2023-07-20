1e27ef18b35d99436e84ae6ca7ece84b08c11691
Andres Pulido <afpulido509@hotmail.com>
2023-07-19 19:44:35 -0500

    Agregar archivos de arranque

---
+++ controllers/Landing.php
@@ -0,0 +1,13 @@
+<?php 
+    // Clase principal que arranca el código
+    class Landing{        
+        public function __construct(){}
+        public function main(){
+            echo "Estoy en el controlador Landing 
+            y funcion principal";
+            //require_once "views/company/header.view.php";
+            //require_once "views/company/index.view.php";
+            //require_once "views/company/footer.view.php";
+        }
+    }
+?>
